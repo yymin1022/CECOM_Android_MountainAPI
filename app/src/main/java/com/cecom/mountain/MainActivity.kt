c7d8fbd840c4d9ae5f0209289c60e9b6578315d5
yymin1022 <yymin1022@gmail.com>
2022-11-13 22:07:42 +0900

    Update MainActivity : Added OnClickListener on Buttons for Opening Detail / List Activity

--- app/src/main/java/com/cecom/mountain/MainActivity.kt
+++ app/src/main/java/com/cecom/mountain/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.cecom.mountain
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnOpenDetail = findViewById<Button>(R.id.main_btn_open_detail)
+        val btnOpenList = findViewById<Button>(R.id.main_btn_open_list)
+        btnOpenDetail.setOnClickListener(btnListener())
+        btnOpenList.setOnClickListener(btnListener())
+    }
+
+    inner class btnListener: View.OnClickListener {
+        override fun onClick(v: View?) {
+            when(v?.id){
+                R.id.main_btn_open_detail -> startActivity(Intent(applicationContext, MountainDetailActivity::class.java))
+                R.id.main_btn_open_list -> startActivity(Intent(applicationContext, MountainListActivity::class.java))
+            }
+        }
     }
 }
