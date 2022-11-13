6453606536345a6676787bd2f345dd79ba1cebf1
yymin1022 <yymin1022@gmail.com>
2022-11-13 21:57:51 +0900

    Update Project : Added MountainDetailActivity

---
+++ app/src/main/java/com/cecom/mountain/MountainDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.cecom.mountain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MountainDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mountain_detail)
+    }
+}
